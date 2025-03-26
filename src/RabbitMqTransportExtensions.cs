e063531d3fd42d14dafbd0793b9575a694eb62c7
Gina Therese Hvidsten <8248605+gthvidsten@users.noreply.github.com>
2025-03-26 14:48:28 +0100

    Added methods to read from configuration

--- src/RabbitMqTransportExtensions.cs
+++ src/RabbitMqTransportExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Whitestone.Cambion.Interfaces;
@@ -10,13 +11,35 @@ namespace Whitestone.Cambion.Transport.RabbitMQ
         // ReSharper disable once InconsistentNaming
         public static ICambionSerializerBuilder UseRabbitMqTransport(this ICambionTransportBuilder builder, string connectionString)
         {
-            return builder.UseRabbitMqTransport(conf => conf.Connection.ConnectionString = new Uri(connectionString));
+            return UseRabbitMqTransport(builder, conf => conf.Connection.ConnectionString = new Uri(connectionString));
+        }
+
+        public static ICambionSerializerBuilder UseRabbitMqTransport(this ICambionTransportBuilder builder, Action<RabbitMqConfig> configure)
+        {
+            return UseRabbitMqTransport(builder, null, configure, null);
+        }
+
+        public static ICambionSerializerBuilder UseRabbitMqTransport(this ICambionTransportBuilder builder, IConfiguration configuration, string cambionConfigurationKey = "Cambion")
+        {
+            return UseRabbitMqTransport(builder, configuration, null, cambionConfigurationKey);
         }
 
         // ReSharper disable once InconsistentNaming
-        public static ICambionSerializerBuilder UseRabbitMqTransport(this ICambionTransportBuilder builder, Action<RabbitMqConfig> configure)
+        public static ICambionSerializerBuilder UseRabbitMqTransport(this ICambionTransportBuilder builder, IConfiguration configuration, Action<RabbitMqConfig> configure, string cambionConfigurationKey = "Cambion")
         {
             builder.Services.Replace(new ServiceDescriptor(typeof(ITransport), typeof(RabbitMqTransport), ServiceLifetime.Singleton));
+
+            if (configuration != null)
+            {
+                string assemblyName = typeof(RabbitMqTransport).Assembly.GetName().Name;
+
+                IConfigurationSection config = configuration.GetSection(cambionConfigurationKey).GetSection("Transport").GetSection(assemblyName);
+
+                if (config.Exists())
+                {
+                    builder.Services.Configure<RabbitMqConfig>(config);
+                }
+            }
 
             builder.Services.AddOptions<RabbitMqConfig>()
                 .Configure(conf => { configure?.Invoke(conf); });
